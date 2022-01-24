a94e3d4627477c7739861e330fb90b27554d1d4c
SitiKarmila <90734242+SitiKarmila@users.noreply.github.com>
2022-01-24 15:35:03 +0700

    TugasDPW2: Laravel Data Properties Manipulation

---
+++ tugas9/system/resources/views/admin/user/show.blade.php
@@ -0,0 +1,30 @@
+@extends('admin.template.app')
+@section('title', 'Detail User')
+
+@section('content')
+
+<div class="container">
+        <div class="row ">
+            <div class="col-md-12 mt-5">
+                <div class="card">
+                    <div class="card-header">
+                        Detail User
+                    </div>
+                    <div class="card-body">
+                        <h3>{{$user->nama}}</h3>
+                            <hr>
+                            <p>
+                            {{"@".$user->username}} |
+                            Email : {{$user->email}} 
+                            </p>
+                            <p>
+                            No Handphone : {{$user->detail->no_handphone}} 
+                            </p>
+                            <hr>
+                    </div>
+                </div>           
+            </div>
+        </div>
+    </div>
+
+@endsection
